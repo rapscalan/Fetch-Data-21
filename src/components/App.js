c73d031af7a785ba5f2c45767f20e19f21d94153
Zak Hyslin <rapscalan@gmail.com>
2020-02-04 14:04:52 -0800

    travis

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import QuoteFetcher from '../containers/QuoteFetcher';
-import QuoteFetcherFn from '../containers/QuoteFetcherFn';
+//import QuoteFetcher from '../containers/QuoteFetcher';
+import QuoteFetcher from '../containers/QuoteFetcherFn';
 
 export default function App() {
-  return <QuoteFetcherFn />;
+  return <QuoteFetcher />;
 }
